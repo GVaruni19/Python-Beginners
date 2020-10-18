f7435def5d1dfeecbda95e2b2e628c7593ffce81
Anthima <58143009+Anthima@users.noreply.github.com>
2020-10-18 19:19:51 +0530

    Create 3.py
    3rd program add two numbers

---
+++ 3.py
@@ -0,0 +1,12 @@
+// Program to Add Two Numbers from user
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+ 
+sum = a + b
+ 
+print("sum:", sum)
+
+//add two numbers
+x = 5
+y = 10
+print(x + y)
