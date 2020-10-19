3671db80d2b6264937e454637688acc5071664e5
SreenivasDheeraj <58567061+SreenivasDheeraj@users.noreply.github.com>
2020-10-19 09:36:56 +0530

    ....

--- 16.py
+++
@@ -1,7 +0,0 @@
-character = input("Please enter a character -->")
-
-if ((character >= "A" and character <='Z') or (character >= "a" and character <='z')):
-    print(character, "is in the alphabet")
-
-else:
-    print (character, "is not in the alphabet")
